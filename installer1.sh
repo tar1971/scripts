90a6c9bd207007bee4be46a028cb05dd5be5baa8
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-05-15 23:08:20 +0200

    Update installer1.sh

--- installer1.sh
+++ installer1.sh
@@ -2,11 +2,11 @@
 #
 #command wget https://raw.githubusercontent.com/tar1971/scripts/main/installer1.sh -O - | /bin/sh
 
-wget -O /var/volatile/tmp/T-DDlseverall.sh.tar "https://raw.githubusercontent.com/emil237/scripts/main/script.tar.gz"
+wget -O /var/volatile/tmp/T-DDlseverall.sh.tar "https://github.com/tar1971/scripts/blob/main/T-DDlseverall.sh.tar.gz?raw=true"
 wait
 tar xzvpf /tmp/*.tar.gz  -C /
 wait
-rm -r /var/volatile/tmp/script.tar.gz                         
+rm -r /var/volatile/tmp/T-DDlseverall.sh.tar.gz                         
 
 echo "========================================================================"
 echo " chmod  all scripts "
@@ -16,7 +16,7 @@ chmod 755 /usr/script/*.sh
 	echo ".        WELCOME TO SCRIPTS UPDATE "
 	
 echo "#    SCRIPT INSTALLED SUCCESSFULLY           #"
-echo "        UPLOADED BY EMIL_NABIL  "     
+echo "        UPLOADED BY TAR_TAR  "     
 echo "#########################################################"
 echo "#########################################################"
 wait
