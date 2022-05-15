af2355b478de606ab4538a380095e7124b19164c
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-05-16 00:03:19 +0200

    Add files via upload

---
+++ installer2.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+#command wget https://raw.githubusercontent.com/tar1971/scripts/main/installer2.sh -O - | /bin/sh
+
+wget -O /var/volatile/tmp/T-DDlseverall.sh.tar "https://github.com/tar1971/scripts/blob/main/T-DDlseverall.sh.tar.gz?raw=true"
+wait
+tar xzvpf /tmp/*.tar.gz  -C /
+wait
+rm -r /var/volatile/tmp/T-DDlseverall.sh.tar.gz                         
+
+echo "========================================================================"
+echo " chmod  all scripts "
+
+chmod 755 /usr/script/*.sh
+                
+	echo ".        WELCOME TO SCRIPTS UPDATE "
+	
+echo "#    SCRIPT INSTALLED SUCCESSFULLY           #"
+echo "        UPLOADED BY TAR_TAR  "     
+echo "#########################################################"
+echo "#########################################################"
+wait
+sleep 2
+exit 0
+
+
+
+
+
